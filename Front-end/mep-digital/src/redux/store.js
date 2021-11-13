0df5dd3fdab70b7021eedba179c8418b679374f3
Esteban <estebanguzra@gmail.com>
2021-11-13 11:40:42 -0600

    teachers reducer for admin_teacher

--- Front-end/mep-digital/src/redux/store.js
+++ Front-end/mep-digital/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { courseReducer } from './courseReducer';
 import { userReducer} from './userReducer';
+import { teachersReducer } from './teachersReducer'
 
 export const store = configureStore({
   reducer: {
     userInfo: userReducer,
-    coursesInfo: courseReducer
+    coursesInfo: courseReducer,
+    teachersInfo: teachersReducer
   },
 });
---
+++ Front-end/mep-digital/src/redux/teachersReducer.js
@@ -0,0 +1,62 @@
+
+const initialState = {
+    teachers: [],
+    selectTeacher: {},
+    newFlag: false
+  };
+  
+  export const teachersReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case "GET_TEACHERS":
+        return state;
+      case "UPDATE_TEACHERS":
+        let newSelectTeacher = {}
+        if(state.selectTeacher !== undefined && !state.newFlag){ //if someone is selected and not a new teacher
+          newSelectTeacher = action.data.teachers.find(teacher => teacher.id === state.selectTeacher.id) //updte selected
+        }
+        return {
+          ...state,
+          newFlag:false,
+          teachers: action.data.teachers,
+          selectTeacher: newSelectTeacher
+        };
+      case "SELECT_TEACHER":
+        return{
+          ...state,
+          newFlag:false,
+          selectTeacher: state.teachers.find(teacher => teacher.id === action.data.idTeacher)
+        }
+      case "SELECT_NEW_TEACHER":
+        return{
+          ...state,
+          newFlag:true,
+          selectTeacher:{
+            name:"",
+            id:"",
+            lastname:"",
+            ratings:[]
+          }
+        }
+      default:
+        console.log("No registred action");
+        console.log(action);
+        return state;
+    }
+  };
+  
+  export function createAction(data, type) {
+    return {
+      type: type,
+      data: data,
+    };
+  }
+  export function updateTeachersAction(teachers) {
+    let data = { teachers: teachers };
+    return createAction(data, "UPDATE_TEACHERS");
+  }
+  export function selectTeacherAction(idTeacher){
+    return createAction({idTeacher},"SELECT_TEACHER")
+  }
+  export function selectNewTeacherAction(){
+    return createAction({},"SELECT_NEW_TEACHER")
+  }
