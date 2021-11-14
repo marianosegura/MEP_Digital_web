7864289086e3840256eb1be9a2130e974e1e8b0c
Esteban <estebanguzra@gmail.com>
2021-11-13 21:44:28 -0600

    Assignments ready

--- Front-end/mep-digital/src/components/Assignments.jsx
+++ Front-end/mep-digital/src/components/Assignments.jsx
@@ -1,9 +1,126 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useState } from 'react'
+import { Box } from '@mui/system'
+import { Button, TextField } from '@mui/material'
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import AssignmentsItem from './AssignmentsItem'
 
-export default function Assignments() {
-  return (
-    <div>
-      
-    </div>
+export default function Assignments(props) {
+  const type = useSelector(state => state.userInfo.type)
+  const courseId = useSelector(state => state.coursesInfo.selectCourse.id)
+  const assignments = useSelector(state => state.coursesInfo.selectCourse.assignments)
+  const [date, setDate] = useState(new Date())
+  
+  const [message, setMessage] = useState("")
+  const [title, setTitle] = useState("")
+  const sendAssignments = (e) => {
+    e.preventDefault();
+    var data = {
+        title: title,
+        description: message,
+        submitDate: date
+    };
+    var direction = "https://desolate-everglades-59280.herokuapp.com/api/courses/" + courseId + "/assignments"
+    var myInit = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    };
+    var myRequest = new Request(
+        direction,
+        myInit
+    );
+    fetch(myRequest)
+    .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return response.text().then(text => {throw new Error(text)})
+    })
+    .then((json) => {
+        alert(json.description)
+        props.getCourses()
+        setTitle("")
+        setMessage("")
+    })
+    .catch((error) => {
+      alert(JSON.parse(error.message).message)
+    })
+  }
+  return(
+  <div className="courseBasicInfo">
+  <p>Tareas</p>
+  {type === "teacher" && (
+  <Box sx={{ maxWidth: 300 }}>
+      <TextField
+        id="outlined-basic"
+        inputProps={{ min: 0, style: { textAlign: "center" } }}
+        label="Titulo de la tarea"
+        variant="outlined"
+        name="id"
+        onChange={(e) => {setTitle(e.target.value)}}
+        value={title}
+        fullWidth={true}
+      />
+      <br />
+      <br />
+      <TextField
+        id="courseName"
+        label="Tarea"
+        multiline
+        rows={4}
+        variant="outlined"
+        name="news"
+        onChange={(e) => {setMessage(e.target.value)}}
+        value={message}
+        fullWidth={true}
+      />
+      <br />
+      <br />
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <DatePicker
+        label="Basic example"
+        value={date}
+        onChange={(newValue) => {
+          setDate(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+      </LocalizationProvider>
+      <br />
+      <br />
+      <Button 
+      variant="contained" 
+      onClick={sendAssignments}
+      fullWidth={true}
+      >
+        Enviar
+      </Button>
+    </Box>
+  )}
+
+  {assignments !== undefined && assignments.length > 0 ?
+  (
+    assignments.map((assignmentsItem) => {
+      return(
+        <AssignmentsItem 
+        getCourses={props.getCourses}
+        title={assignmentsItem.title}
+        message={assignmentsItem.description}
+        date={assignmentsItem.submitDate}
+        id={assignmentsItem._id}
+        key={assignmentsItem._id}
+        />
+      )
+    })
+  ):(
+    <p>Sin tareas</p>
   )
+  }
+  
+</div>
+)
 }
--- Front-end/mep-digital/src/components/AssignmentsItem.jsx
+++ Front-end/mep-digital/src/components/AssignmentsItem.jsx
@@ -3,12 +3,12 @@ import React from "react";
 import { useSelector } from "react-redux";
 import DeleteIcon from '@mui/icons-material/Delete';
 
-export default function NewsItem(props) {
+export default function AssignmentsItem(props) {
   const type = useSelector((state) => state.userInfo.type);
   const courseId = useSelector(state => state.coursesInfo.selectCourse.id)
   const handleClick = (e) => {
     e.preventDefault()
-        var direction = "https://desolate-everglades-59280.herokuapp.com/api/courses/" + courseId + "/news/" + props.id //id = _id
+        var direction = "https://desolate-everglades-59280.herokuapp.com/api/courses/" + courseId + "/assignments/" + props.id //id = _id
         var myInit = {
             method: "DELETE",
             headers: { "Content-Type": "application/json" },
@@ -25,7 +25,7 @@ export default function NewsItem(props) {
             return response.json(); //we only get here if there is no error
         })
         .then((json) => {
-            alert(json.message)
+            alert(json.description)
             props.getCourses()
         });
   };
